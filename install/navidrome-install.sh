1f52e1e011079e1cc757e95a22a1b493314edd87
observer96 <40547734+observer96@users.noreply.github.com>
2026-01-09 22:58:33 +0100

    Redirect URLs to private repo.

--- install/navidrome-install.sh
+++ install/navidrome-install.sh
@@ -25,7 +25,7 @@ msg_ok "Started Navidrome"
 
 read -p "${TAB3}Do you want to install filebrowser addon? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/addon/filebrowser.sh)"
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/observer96/ProxmoxVE/refs/heads/nvidia-regression/tools/addon/filebrowser.sh)"
 fi
 
 motd_ssh
